004a39ac3c39873b54a0ab03f25865409fa0122c
Eduardox3703 <eduardoxcelaya@gmail.com>
2025-04-30 16:49:49 -0600

    first commit

---
+++ epk-data.ts
@@ -0,0 +1,136 @@
+export const EPK_DATA = {
+    // "audios-promocionales": [
+    //     {
+    //         title: "Te Va Doler",
+    //         resources: [
+    //             {
+    //                 name: "Te_Va_A_Doler_Mastered.wav",
+    //                 link: "https://drive.google.com/file/d/1OEntjqMG_kZ4-ot0yaFKUsDymo2uu_4j/view?usp=sharing",
+    //             },
+    //         ],
+    //     },
+    //     {
+    //         title: "Mi Persona Preferida",
+    //         resources: [
+    //             {
+    //                 name: "Mi_Persona_Preferida_Mastered.wav",
+    //                 link: "https://drive.google.com/file/d/1EkPAHqI9cY7_hN7mO6sa5cEQdhg0sUPh/view?usp=sharing",
+    //             },
+    //         ],
+    //     },
+    //     {
+    //         title: "Tal Vez",
+    //         resources: [
+    //             {
+    //                 name: "Tal_Vez_Master_Mastered.wav",
+    //                 link: "https://drive.google.com/file/d/1QlulPx24e-VAYoCkvI3gtmCOdQO6sXyE/view?usp=sharing",
+    //             },
+    //         ],
+    //     },
+    // ],
+    "fotos-oficiales": [
+        {
+            title: null,
+            resources: [
+                {
+                    name: "LosPrimos_edit03.png",
+                    link: "https://drive.google.com/file/d/19KBlgiYEGy-5hJFOpeV4AJJywTnnqFpz/view?usp=sharing"
+                },
+                {
+                    name: "LosPrimos_edit03.jpg",
+                    link: "https://drive.google.com/file/d/10QjVYjxE2pI-KIGMJ3iZ-a8AUB3Yo7iw/view?usp=sharing"
+                
+                },
+                {
+                    name: "LosPrimos_edit02.png",
+                    link: "https://drive.google.com/file/d/1j2dx0F-4rH7BmTln3D5fSeSMm-F3kd0i/view?usp=sharing"
+                },
+                {
+                    name: "LosPrimos_edit02.jpg",
+                    link: "https://drive.google.com/file/d/1HSwVwFw-pVVNx4nL0ythugp5NqJuz1XC/view?usp=sharing"
+                
+                },
+                {
+                    name: "LosPrimos_edit01.png",
+                    link: "https://drive.google.com/file/d/10qQbdWe9HPNVhjkSsCkzwSXl8dF5XU8G/view?usp=sharing"
+                },
+                {
+                    name: "LosPrimos_edit01.jpg",
+                    link: "https://drive.google.com/file/d/1gbhF5owY7pgWXXqRSg40MZParZbTYs1D/view?usp=sharing"
+                
+                },
+            ],
+        },
+    ],
+    "logotipo-oficial": [
+        {
+            title: null,
+            resources: [
+                {
+                    name: "LOGO LOS PRIMOS.png",
+                    link: "https://drive.google.com/file/d/1DAcDWbtVghku07Aq7F7HoT8numResw65/view?usp=sharing",
+                },
+                {
+                    name: "LOGO LOS PRIMOS BLANCO.png",
+                    link: "https://drive.google.com/file/d/1Z69FiwXP51P-Qi58xxIF1aDx9sI26NeT/view?usp=sharing"
+                },
+            ],
+        },
+    ],
+    "rider-tecnico": [
+        {
+            title: null,
+            resources: [
+                {
+                    name: "archivo-ejemplo.pdf",
+                    link: "https://9l6qnyfkes.ufs.sh/f/oCuvxwbj7yrx2q0R1Lun0Rje3hU5TWIDNfbVEsPL7aB98Xkc"
+                }
+            ]
+        }
+    ],
+    "brochure": [
+        {
+            title: null,
+            resources: [
+                {
+                    name: "archivo-ejemplo.pdf",
+                    link: "https://9l6qnyfkes.ufs.sh/f/oCuvxwbj7yrx2q0R1Lun0Rje3hU5TWIDNfbVEsPL7aB98Xkc"
+                }
+            ]
+        }
+    ],
+    biografia: [
+        {
+            title: null,
+            resources: [
+                {
+                    name: "archivo-ejemplo.pdf",
+                    link: "https://9l6qnyfkes.ufs.sh/f/oCuvxwbj7yrx2q0R1Lun0Rje3hU5TWIDNfbVEsPL7aB98Xkc",
+                },
+            ],
+        },
+    ],
+    "videos-oficiales": [
+        {
+            title: null,
+            resources: [
+                {
+                    name: "archivo-ejemplo.mp4",
+                    link: "https://9l6qnyfkes.ufs.sh/f/oCuvxwbj7yrx2q0R1Lun0Rje3hU5TWIDNfbVEsPL7aB98Xkc",
+                },
+            ],
+        },
+    ],
+};
+
+export const EPK_VIEWS_NAMES: Record<string, string> = {
+    // "audios-promocionales": "Audios Promocionales",
+    "fotos-oficiales": "Fotos Oficiales",
+    "logotipo-oficial": "Logotipo Oficial",
+    "videos-oficiales": "Videos Oficiales",
+    // "temas-promocionales": "Temas Promocionales",
+    "rider-tecnico": "Rider Técnico",
+    "brochure": "Brochure",
+    biografia: "Biografía",
+    // "otros-elementos": "Otros Elementos",
+};
