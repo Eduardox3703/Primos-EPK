6059db14783c93427b02b4ea75f7c39962f75fb9
Eduardox3703 <eduardoxcelaya@gmail.com>
2025-10-03 14:10:39 -0600

    Actualizacion de EPK de primos de durango

--- epk-data.ts
+++ epk-data.ts
@@ -1,33 +1,96 @@
 export const EPK_DATA = {
-    // "audios-promocionales": [
-    //     {
-    //         title: "Te Va Doler",
-    //         resources: [
-    //             {
-    //                 name: "Te_Va_A_Doler_Mastered.wav",
-    //                 link: "https://drive.google.com/file/d/1OEntjqMG_kZ4-ot0yaFKUsDymo2uu_4j/view?usp=sharing",
-    //             },
-    //         ],
-    //     },
-    //     {
-    //         title: "Mi Persona Preferida",
-    //         resources: [
-    //             {
-    //                 name: "Mi_Persona_Preferida_Mastered.wav",
-    //                 link: "https://drive.google.com/file/d/1EkPAHqI9cY7_hN7mO6sa5cEQdhg0sUPh/view?usp=sharing",
-    //             },
-    //         ],
-    //     },
-    //     {
-    //         title: "Tal Vez",
-    //         resources: [
-    //             {
-    //                 name: "Tal_Vez_Master_Mastered.wav",
-    //                 link: "https://drive.google.com/file/d/1QlulPx24e-VAYoCkvI3gtmCOdQO6sXyE/view?usp=sharing",
-    //             },
-    //         ],
-    //     },
-    // ],
+    "audios-promocionales": [
+        {
+            title: "Tu Magia",
+            resources: [
+                {
+                    name: "Los Primos MX - TuMagia.wav",
+                    link: "https://drive.google.com/file/d/1Iybu-T_EZ6cjJx8Sbup1VGoBtxUOFYTA/view?usp=sharing",
+                },
+            ],
+        },
+        {
+            title: "Mañana",
+            resources: [
+                {
+                    name: "PRIMOS_Mañana_MASTERED_44k-16b.wav",
+                    link: "https://drive.google.com/file/d/1kFoqF2tJMZsfOsxOrFhnGHv7v9m2JvG2/view?usp=sharing",
+                },
+            ],
+        },
+        {
+            title: "Mienteme feat Erick Rincon",
+            resources: [
+                {
+                    name: "PRIMOS_Mienteme_48k-24b-Para-VIDEO.wav",
+                    link: "https://drive.google.com/file/d/1M2ym8T6yYYBZ3JpFUlNlor0XgTKgb9yV/view?usp=sharing",
+                },
+            ],
+        },
+        {
+            title: "Tal Vez",
+            resources: [
+                {
+                    name: "Los-Primos-mx-feat-Ponzoña-Musical-Tal-Vez.mp3",
+                    link: "https://drive.google.com/file/d/1IsqzI9qyQ6yy9YMZxAm92svsxp4x7xJN/view?usp=sharing",
+                },
+            ],
+        },
+        {
+            title: "Y Tu Te Vas",
+            resources: [
+                {
+                    name: "PRIMOS_Y Tu Te Vas_MASTERED_44k-16b.wav",
+                    link: "https://drive.google.com/file/d/15YmKUkMVkP5p5BlwkuadwyhOQBOgeD5E/view?usp=sharing",
+                },
+            ],
+        },
+        {
+            title: "Mi Bello Angel",
+            resources: [
+                {
+                    name: "PRIMOS_Mi Bello Angel_MASTERED_44k-16b.wav",
+                    link: "https://drive.google.com/file/d/1B70pWtSx22d3FNb2z-nfJbzJPRUfNysB/view?usp=sharing",
+                },
+            ],
+        },
+        {
+            title: "Te Perdonaria",
+            resources: [
+                {
+                    name: "PRIMOS_Te Perdonaria_MASTERED_44k-16b.wav",
+                    link: "https://drive.google.com/file/d/1MdaHoyxXi3OMQ98DoS5llAQie_xqkEgv/view?usp=sharing",
+                },
+            ],
+        },
+        {
+            title: "Fuego En Tu Piel",
+            resources: [
+                {
+                    name: "PRIMOS_Fuego En Tu Piel_MASTERED_44k-16b.wav",
+                    link: "https://drive.google.com/file/d/130RE5E6q9Yv9cuLHLKgAPgdZVnVWCCk3/view?usp=sharing",
+                },
+            ],
+        },
+        {
+            title: "Celoso",
+            resources: [
+                {
+                    name: "02 Cesolo.wav",
+                    link: "https://drive.google.com/file/d/1lLqbgI07E4M74aj73R3-peT1y-QU2AHJ/view?usp=sharing",
+                },
+            ],
+        },
+        // {
+        //     title: "Almas Gemelas feat El Trono De Mexico",
+        //     resources: [
+        //         {
+        //             name: "AlmasGemelas.mov",
+        //             link: "https://drive.google.com/file/d/1bfCFJkx7kuYEXKzLblWvmHIReLbSFNNp/view?usp=sharing",
+        //         },
+        //     ],
+        // },
+    ],
     "fotos-oficiales": [
         {
             title: null,
@@ -82,7 +145,7 @@ export const EPK_DATA = {
             title: null,
             resources: [
                 {
-                    name: "archivo-ejemplo.pdf",
+                    name: "LOS PRIMOSMXRIDER.pdf",
                     link: "https://9l6qnyfkes.ufs.sh/f/oCuvxwbj7yrx2q0R1Lun0Rje3hU5TWIDNfbVEsPL7aB98Xkc"
                 }
             ]
@@ -93,38 +156,128 @@ export const EPK_DATA = {
             title: null,
             resources: [
                 {
-                    name: "archivo-ejemplo.pdf",
-                    link: "https://9l6qnyfkes.ufs.sh/f/oCuvxwbj7yrx2q0R1Lun0Rje3hU5TWIDNfbVEsPL7aB98Xkc"
+                    name: "LosPrimos deDurango_Brochure.pdf",
+                    link: "https://drive.google.com/file/d/1oDWgnAp-guYyE-RlBklNFOydApvub-Sj/view?usp=sharing"
                 }
             ]
         }
     ],
-    biografia: [
+    "biografia": [
         {
             title: null,
             resources: [
                 {
-                    name: "archivo-ejemplo.pdf",
-                    link: "https://9l6qnyfkes.ufs.sh/f/oCuvxwbj7yrx2q0R1Lun0Rje3hU5TWIDNfbVEsPL7aB98Xkc",
+                    name: "BiografiaPrimoxMX2024.pdf",
+                    link: "https://drive.google.com/file/d/1EHy0Pf8jeXerkaDzQDmUznCte0O3mFHo/view?usp=sharing",
                 },
             ],
         },
     ],
     "videos-oficiales": [
         {
-            title: null,
+            title: "Tu Magia",
             resources: [
                 {
-                    name: "archivo-ejemplo.mp4",
-                    link: "https://9l6qnyfkes.ufs.sh/f/oCuvxwbj7yrx2q0R1Lun0Rje3hU5TWIDNfbVEsPL7aB98Xkc",
+                    name: "Los Primos Mx - Tu Magia - H264.mp4",
+                    link: "https://drive.google.com/file/d/1V6Q9c_06reXLh35tKbA555u9P140xX8z/view?usp=sharing",
                 },
             ],
         },
+        {
+            title: "Mañana",
+            resources: [
+                {
+                    name: "Los Primos de Durango - Mañana.mp4",
+                    link: "https://drive.google.com/file/d/1wYXFDoDhH1b3XkrJwp56rDDpOMRcypEp/view?usp=sharing",
+                },
+            ],
+        },
+        {
+            title: "Mienteme feat Erick Rincon",
+            resources: [
+                {
+                    name: "Los Primos de Durango - Mienteme.mp4",
+                    link: "https://drive.google.com/file/d/19rT0f6LdHSss8JkAbSos-vgAfRfjhR-h/view?usp=sharing",
+                },
+            ],
+        },
+        {
+            title: "Tal Vez",
+            resources: [
+                {
+                    name: "Los Primos de Durango  - Tal Vez.mp4",
+                    link: "https://drive.google.com/file/d/1wtB3MUie5qMd5_vlDdQtDcNTZJErF7Gf/view?usp=sharing",
+                },
+            ],
+        },
+        {
+            title: "Y Tu Te Vas",
+            resources: [
+                {
+                    name: "Los Primos de Durango - Y Tu Te Vas.mp4",
+                    link: "https://drive.google.com/file/d/1fuBTfSWXMmITXl-k6bBHzjZljgeEl98B/view?usp=sharing",
+                },
+            ],
+        },
+        {
+            title: "Mi Bello Angel",
+            resources: [
+                {
+                    name: "Los_Primos_MX_Mi_Bello_Angel_H264.mp4",
+                    link: "https://drive.google.com/file/d/1bNozaSp9EAYHCiJ-oXos1RTU9BYcoX4L/view?usp=sharing",
+                },
+            ],
+        },
+        {
+            title: "Te Perdonaria",
+            resources: [
+                {
+                    name: "Los Primos De Durango - Te Perdonaria.mp4",
+                    link: "https://drive.google.com/file/d/1zTVdqWpiVaL_OUzis5EwJBSihIo1aX4X/view?usp=sharing",
+                },
+            ],
+        },
+        {
+            title: "Fuego En Tu Piel",
+            resources: [
+                {
+                    name: "Los Primos de Durango - Fuego En Tu Piel.mp4",
+                    link: "https://drive.google.com/file/d/11At1VbvBliAr5CTIl60IRTuFQSgQUIMp/view?usp=sharing",
+                },
+            ],
+        },
+        {
+            title: "Celoso",
+            resources: [
+                {
+                    name: "Los Primos De Durango - Celoso.mp4",
+                    link: "https://drive.google.com/file/d/1LwKvJPkDQjLSZYziF3Bvy_efM9O3NjmW/view?usp=sharing",
+                },
+            ],
+        },
+        {
+            title: "Almas Gemelas feat El Trono De Mexico",
+            resources: [
+                {
+                    name: "AlmasGemelas.mov",
+                    link: "https://drive.google.com/file/d/1bfCFJkx7kuYEXKzLblWvmHIReLbSFNNp/view?usp=sharing",
+                },
+            ],
+        },
+        // {
+        //     title: "Tu Magia",
+        //     resources: [
+        //         {
+        //             name: "Los Primos Mx - Tu Magia - H264.mp4",
+        //             link: "https://drive.google.com/file/d/1V6Q9c_06reXLh35tKbA555u9P140xX8z/view?usp=sharing",
+        //         },
+        //     ],
+        // },
     ],
 };
 
 export const EPK_VIEWS_NAMES: Record<string, string> = {
-    // "audios-promocionales": "Audios Promocionales",
+    "audios-promocionales": "Audios Promocionales",
     "fotos-oficiales": "Fotos Oficiales",
     "logotipo-oficial": "Logotipo Oficial",
     "videos-oficiales": "Videos Oficiales",
